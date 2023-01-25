1b55cee8c5cca17ccaef2b5ed553efaff8819241
Josh Burch <joshuaburch01@gmail.com>
2023-01-25 15:33:48 -0600

    added tables_exercise file

---
+++ tables_exercises.sql
@@ -0,0 +1,17 @@
+SHOW CREATE DATABASE employees;
+USE employees;
+SHOW Tables;
+DESCRIBE employees;
+ -- int, date carchar(14), varchar(16), enum, and date
+-- Which table(s) do you think contain a numeric type column? emp_no
+-- Which table(s) do you think contain a string type column? first_name, last_name, gender
+-- Which table(s) do you think contain a date type column? birth_date, hire-date
+DESCRIBE departments;
+DESCRIBE dept_emp;
+SELECT * FROM dept_emp;
+SELECT * FROM employees;
+SELECT * FROM departments;
+
+
+-- What is the relationship between the employees and the departments tables? Can't find a realationship. 
+SHOW CREATE TABLE dept_manager;
