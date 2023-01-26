29d7845675b41077fa2839bc0ccdefdc5f4e7269
Josh Burch <joshuaburch01@gmail.com>
2023-01-26 10:47:21 -0600

    fixed/corrected a problem

--- where_exercises.sql
+++ where_exercises.sql
@@ -5,7 +5,7 @@ SELECT * FROM employees;
 
 SELECT first_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya'); -- 709
 SELECT first_name FROM employees WHERE first_name = 'Irena'OR 'Vidya' OR 'Maya'; -- 241 
-SELECT * FROM employees WHERE first_name = ('Irena'OR 'Vidya' OR 'Maya') and gender = 'M'; -- 179973
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') and gender = 'M'; -- 179973
 SELECT * FROM employees WHERE last_name LIKE 'E%'; -- 7330
 SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E'; -- 30723 starts and ends with E
 SELECT * FROM employees WHERE last_name LIKE '%E'; -- ends with E
