9ddc39cba76d717b87828cfbdb6c771b355b2629
Josh Burch <joshuaburch01@gmail.com>
2023-02-02 13:51:17 -0600

    Fixed some questions to add subqueries

--- subqueries_exercises'.sql
+++ subqueries_exercises'.sql
@@ -10,23 +10,27 @@ SELECT * FROM dept_emp LIMIT 10;
         FROM employees
         WHERE emp_no = 101010);
 
--- 2. 
+-- 2.
+	SELECT * FROM (
 	SELECT title, first_name, employees.emp_no FROM employees 
 	JOIN titles ON titles.emp_no = employees.emp_no
     JOIN dept_emp ON dept_emp.emp_no = titles.emp_no
     WHERE employees.first_name = 'Aamod' AND
-    dept_emp.to_date LIKE '9999%';
+    dept_emp.to_date LIKE '9999%') AS kdja;
     
 -- 3. 
-	SELECT COUNT(*) FROM dept_emp
-    WHERE dept_emp.to_date NOT LIKE '9999%'; -- 91479 
+	SELECT * FROM 
+    (Select COUNT(*) FROM dept_emp
+    WHERE dept_emp.to_date 
+    NOT LIKE '9999%')AS kd; -- 91479 
     
 -- 4. 
+	SELECT * FROM (
 	SELECT departments.dept_name,gender,
         CONCAT(employees.first_name, ' ', employees.last_name) AS Manager_name FROM employees 
         JOIN dept_manager ON dept_manager.emp_no = employees.emp_no 
         JOIN departments ON departments.dept_no = dept_manager.dept_no
-        WHERE employees.gender LIKE 'F%' AND dept_manager.to_date LIKE '9999%';
+        WHERE employees.gender LIKE 'F%' AND dept_manager.to_date LIKE '9999%') as A;
 	
 -- 5.
 	SELECT CONCAT(first_name, ' ', last_name) AS employee_name, salary FROM employees
@@ -36,7 +40,9 @@ SELECT * FROM dept_emp LIMIT 10;
     
 -- 6. 
 	SELECT COUNT(*) FROM salaries
-    WHERE salary >= (SELECT MAX(salary) FROM salaries) -  (SELECT stddev(salary) FROM salaries
+    WHERE salary >= (SELECT MAX(salary) FROM salaries 
+    WHERE salaries.to_date LIKE '9999%') - 
+    (SELECT stddev(salary) FROM salaries
 	WHERE salaries.to_date LIKE '9999%')
     AND to_date LIKE '9999%'; 
     
